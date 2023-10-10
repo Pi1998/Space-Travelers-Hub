d708b63c905c1b7e8aa844379e9fbe4f450f03fe
Shinn Thant Swam Ye <1shinnthantswamye@gmail.com>
2023-10-10 20:14:52 +0630

    Add action to store the selected data in Redux store

--- space-travelers-hub/src/components/rockets/Rockets.js
+++ space-travelers-hub/src/components/rockets/Rockets.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchRockets } from '../../redux/rockets/rocketsSlice';
 
-function Rockets() {
+const Rockets = () => {
+  const dispatch = useDispatch();
+  const rockets = useSelector((state) => state.rockets.rockets);
+  const status = useSelector((state) => state.rockets.status);
+  const error = useSelector((state) => state.rockets.error);
+
+  useEffect(() => {
+    if (status === 'idle') {
+      dispatch(fetchRockets());
+    }
+  }, [dispatch, status]);
+
+  if (status === 'loading') {
+    return <div>Loading...</div>;
+  }
+
+  if (status === 'failed') {
+    return (
+      <div>
+        Error:
+        {error}
+      </div>
+    );
+  }
+
   return (
     <div>
-      <h2>Rockets Page</h2>
+      {rockets.map((rocket) => (
+        <div key={rocket.id}>
+          <h3>{rocket.rocket_name}</h3>
+          <p>{rocket.description}</p>
+          <img src={rocket.flickr_images[0]} alt="Rocket" />
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default Rockets;
