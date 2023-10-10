8e8bb568495e12da6e0fef26c32414284029207d
Shinn Thant Swam Ye <1shinnthantswamye@gmail.com>
2023-10-10 13:43:27 +0630

    Add logo img

--- space-travelers-hub/src/components/navigation/Navigation.js
+++ space-travelers-hub/src/components/navigation/Navigation.js
@@ -1,27 +1,42 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import './navigation.css';  
+import logoImage from '../../icon/planet.png';
+import './navigation.css';
+
 
 const Navigation = () => {
   return (
-    <nav>
-      <ul className="nav-links">
-        <li>
-          <NavLink to="/rockets" activeClassName="active">
-            Rockets
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/missions" activeClassName="active">
-            Missions
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/myprofile" activeClassName="active">
-            My Profile
-          </NavLink>
-        </li>
-      </ul>
+    <nav className="navbar">
+      <div className="navbar-left">
+        <div className="logo">
+        <img src={logoImage} alt="Logo" />
+        </div>
+        <div className="site-name">
+          <h2>Space Travelers' Hub</h2>
+        </div>
+      </div>
+      <div className="navbar-right">
+        <ul className="nav-links">
+          <li>
+            <NavLink to="/rockets" activeClassName="active">
+              Rockets
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/missions" activeClassName="active">
+              Missions
+            </NavLink>
+          </li>
+          <li>
+            <div className="vertical-line" />
+          </li>
+          <li>
+            <NavLink to="/myprofile" activeClassName="active">
+              My Profile
+            </NavLink>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
