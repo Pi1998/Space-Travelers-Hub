eed7f47d3c35810e8320e4242c248cd098c8c54e
Shinn Thant Swam Ye <1shinnthantswamye@gmail.com>
2023-10-10 13:20:50 +0630

    Add routes

---
+++ space-travelers-hub/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Rockets from './components/rockets/Rockets';
+import Missions from './components/missions/Mission';
+import MyProfile from './components/myProfile/MyProfile';
+import Navigation from './components/navigation/Navigation';
+import './App.css';
+
+const App = () => {
+  return (
+    <Router>
+      <div className="App">
+        <Navigation />
+        <Routes>
+          <Route path="/rockets" element={<Rockets />} />
+          <Route path="/missions" element={<Missions />} />
+          <Route path="/myprofile" element={<MyProfile />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
