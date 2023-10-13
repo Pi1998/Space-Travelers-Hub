970ba28f472d645c20594fed0d0e17f8169816d7
Esmatullah <esmatqadeemi12@gmail.com>
2023-10-13 08:54:15 +0100

    Fix the linter error

--- space-travelers-hub/src/redux/store.js
+++ space-travelers-hub/src/redux/store.js
@@ -1,20 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
-<<<<<<< HEAD
+import rocketsReducer from './rockets/rocketsSlice';
 import missionsReducer from '../missions/missionsSlice';
 
 const store = configureStore({
   reducer: {
     mission: missionsReducer,
-  },
-});
-=======
-import rocketsReducer from './rockets/rocketsSlice';
-
-const store = configureStore({
-  reducer: {
     rockets: rocketsReducer,
   },
 });
 
->>>>>>> 098d084507bd426f6df3ec71c9f14c6ebc81642e
 export default store;
