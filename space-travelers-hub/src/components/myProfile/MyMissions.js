dfad8419e2e199bb95be02307be743821d5ab488
Esmatullah <esmatqadeemi12@gmail.com>
2023-10-13 19:03:01 +0100

    Style the profile page rockets and missions and also modify the style of the reserved and cancel rokets

--- space-travelers-hub/src/components/myProfile/MyMissions.js
+++ space-travelers-hub/src/components/myProfile/MyMissions.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchMissions } from '../../redux/missions/missionsSlice';
+import './MyMissions.css';
 
 const MyMissions = () => {
   const mission = useSelector((state) => state.mission);
