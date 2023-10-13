64852cdcf540ddd6fb37f09940ba7064c4f5599c
Esmatullah <esmatqadeemi12@gmail.com>
2023-10-13 07:10:01 +0100

    Implement joining and leaving mission functionality

--- space-travelers-hub/src/components/missions/Missions.js
+++ space-travelers-hub/src/components/missions/Missions.js
@@ -1,12 +1,10 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { fetchMissions } from '../../missions/missionsSlice';
+import { cancelMission, fetchMissions, joinMission } from '../../missions/missionsSlice';
 import './Missions.css';
 
 const Missions = () => {
   const mission = useSelector((state) => state.mission);
-  console.log(mission.missions);
-  console.log(mission.loading);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchMissions());
@@ -25,18 +23,18 @@ const Missions = () => {
           <thead className="table-head">
             <th width="10%">Mission</th>
             <th width="62%">Desciption</th>
-            <th width="12%">Status</th>
+            <th width="13%">Status</th>
           </thead>
           <tbody className="table-body">
             {mission.missions.map((mission) => (
               <tr className="table-row" key={mission.mission_id}>
                 <th>{mission.mission_name}</th>
                 <td>{mission.description}</td>
-                <td className="status">{mission.mission_join ? <p>Active Member</p> : <p className="not-memeber">NOT A MEMEBER</p>}</td>
+                <td className="status">{mission.mission_join ? <p className="active-member">Active Member</p> : <p className="not-memeber">NOT A MEMEBER</p> }</td>
                 <td className="btns-col">
                   {mission.mission_join
-                    ? <button className="action-btn" type="button">Leave Mission</button>
-                    : <button className="action-btn" type="button">Join Mission</button>}
+                    ? <button className="leave-btn" type="button" onClick={() => dispatch(joinMission(mission.mission_id))}>Leave Mission</button>
+                    : <button className="join-btn" type="button" onClick={() => dispatch(cancelMission(mission.mission_id))}>Join Mission</button>}
                 </td>
               </tr>
             ))}
